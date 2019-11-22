9045af998c66b0030d8a45ce81ff869fad62bcad
Lorenzo Gabriele <lorenzolespaul@gmail.com>
2019-11-22 15:02:42 +0100

    clean: Use multiple-tests

---
+++ src/main/resources/docs/multiple-tests/no-crash-with-zero-results/src/sloctest.c
@@ -0,0 +1,9 @@
+/* This is a test.  Should produce 6 SLOC.
+ */
+#include <stdio.h>
+#define HI 10
+
+char* main() {
+  int a = 1; /* hi */
+  "hi";
+}
---
+++ src/main/resources/docs/multiple-tests/without-config-file/src/buggy-latin1.c
@@ -0,0 +1,1 @@
+../../../tests/buggy-latin1.c
