a439d191dd36c1cacc332b1106ea66e503736f38
José Miguel  Melo <josemiguelmelo94@gmail.com>
2020-09-15 14:57:46 +0100

    feature: Add enabled by default to patterns.json

---
+++ src/main/scala/codacy/flawfinder/DefaultPatterns.scala
@@ -0,0 +1,31 @@
+package codacy.flawfinder
+
+object DefaultPatterns {
+  val list = List(
+    "_gettc",
+    "_mbslen",
+    "_mbsnbcat",
+    "_mbsnbcpy",
+    "_tcslen",
+    "_tcsncat",
+    "_tcsncpy",
+    "fgetc",
+    "getc",
+    "getchar",
+    "lstrcatn",
+    "lstrcpyn",
+    "memalign",
+    "read",
+    "strcadd",
+    "strccpy",
+    "strlen",
+    "strncat",
+    "strncpy",
+    "ulimit",
+    "umask",
+    "usleep",
+    "wcslen",
+    "wcsncat",
+    "wcsncpy"
+  )
+}
